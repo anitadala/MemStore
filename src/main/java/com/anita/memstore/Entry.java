9d9e5f4c6fbae430cc1847f6b73b7bcfecc89971
anitaKumari <anitadala583@gmail.com>
2026-01-05 17:09:38 +0530

    Add Entry value wrapper

--- src/main/java/com/anita/memstore/Entry.java
+++ src/main/java/com/anita/memstore/Entry.java
@@ -1,1 +1,14 @@
 package com.anita.memstore;
+
+public class Entry<V> {
+    /*wrapped the Entry object to allow future extensions like TTL,metadata or versioning without changing the
+    store API.*/
+    private final V value;
+
+    public Entry(V value){
+        this.value = value;
+    }
+    public V getValue(){
+        return value;
+    }
+}
