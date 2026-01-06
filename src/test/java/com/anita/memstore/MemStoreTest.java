d96a4e9d095632a578ff22f52ec3708590ece6c0
anitaKumari <anitadala583@gmail.com>
2026-01-06 16:14:28 +0530

    Added unit tests for TTL with lazy eviction

--- src/test/java/com/anita/memstore/MemStoreTest.java
+++ src/test/java/com/anita/memstore/MemStoreTest.java
@@ -1,1 +1,52 @@
 package com.anita.memstore;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemStoreTest{
+
+    @Test
+    void shouldReturnValueBeforeTTLExpires(){
+        MemStore<String,String> store = new MemStore<>();
+        store.put("token", "valid", 2000);
+        String value = store.get("token");
+        assertEquals("valid",value);
+    }
+
+    @Test
+    void shouldReturnNullAfterTTLExpires() throws InterruptedException{
+        MemStore<String,String> store = new MemStore<>();
+        store.put("token", "valid", 2000);
+        Thread.sleep(3000);
+        String value = store.get("token");
+        assertNull(value);
+    }
+
+    @Test
+    void shouldLazyEvictExpiredEntry() throws InterruptedException{
+        MemStore<String,String> store = new MemStore<>();
+        store.put("session", "active", 2000);
+        Thread.sleep(3000);
+        // First access triggers eviction
+        assertNull(store.get("session"));
+
+        // Second access confirms eviction
+        assertNull(store.get("session"));
+    }
+
+    @Test
+    void nonTTLValueShouldNeverExpire() throws InterruptedException{
+        MemStore<String,String> store = new MemStore<>();
+        store.put("user", "Anita");
+        Thread.sleep(2000);
+
+        assertEquals("Anita", store.get("user"));
+    }
+
+    @Test
+    void shouldThrowExceptionForInvalidTTL(){
+        MemStore<String, String> store = new MemStore<>();
+        assertThrows(IllegalArgumentException.class,
+                () -> store.put("key", "value", 0));
+    }
+}
